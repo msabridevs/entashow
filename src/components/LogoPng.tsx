3d9dfc0fea1714aa42fac73cba3dfd7b5c359c4f
MSabri AZ <msabridevs@gmail.com>
2026-01-19 01:49:23 +0200

    new logo fixed

--- src/components/LogoPng.tsx
+++ src/components/LogoPng.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LogoPng() {
   return (
     <Image
-      src="/logo.png"
+      src="/logo-v2.png"
       alt="Enta Show"
       width={72}
       height={72}
