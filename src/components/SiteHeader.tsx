f302d50fa9fa9135b4b72a88575d3777f0ba90ed
MSabri AZ <msabridevs@gmail.com>
2026-01-19 02:00:57 +0200

    larger logo2

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -15,7 +15,7 @@ export default function SiteHeader() {
         style={{
           maxWidth: 1200,
           margin: "0 auto",
-          padding: "10px 16px",
+          padding: "20px 24px",
           display: "flex",
           alignItems: "center",
         }}
